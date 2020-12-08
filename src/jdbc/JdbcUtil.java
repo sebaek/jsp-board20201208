3cc6e8d97eb4522fffbf9391498652b5f85b0d64
sebaek <sebaek@gmail.com>
2020-12-08 11:28:33 +0900

    util

--- src/jdbc/JdbcUtil.java
+++ src/jdbc/JdbcUtil.java
@@ -7,9 +7,28 @@ public class JdbcUtil {
 		
 	public static void rollback(Connection con) {
 		try {
-			con.rollback();
+			if (con != null) {
+				con.rollback();
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void close(AutoCloseable... ins) {
+		for (AutoCloseable i : ins) {
+			if (i != null) {
+				try {
+					i.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
+
+
+
+
+
