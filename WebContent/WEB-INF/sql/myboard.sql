618f55ce0720c424228acc59ba6f884370bf87f1
sebaek <sebaek@gmail.com>
2020-12-09 11:40:42 +0900

    service

---
+++ WebContent/WEB-INF/sql/myboard.sql
@@ -0,0 +1,7 @@
+DROP TABLE member;
+CREATE TABLE member (
+    memberid VARCHAR2(50) PRIMARY KEY,
+    name VARCHAR2(50) NOT NULL,
+    password VARCHAR2(10) NOT NULL,
+    regdate DATE NOT NULL
+);
