b0dffe3e3380518ec3e736b8dbaeb598a86ae8bf
sebaek <sebaek@gmail.com>
2020-12-10 17:09:02 +0900

    create

--- WebContent/WEB-INF/sql/myboard.sql
+++ WebContent/WEB-INF/sql/myboard.sql
@@ -23,7 +23,24 @@ CREATE TABLE article_content (
     content VARCHAR2(4000)
 );
 
+INSERT INTO article (write_id,......) 
+VALUES (...);
 
+-- 11g
+INSERT INTO article (article_no, write_id,......) 
+VALUES (article_seq.nextval, ...);
+
+CREATE SEQUENCE article_seq;
+CREATE TABLE article (
+    article_no NUMBER,
+    writer_id VARCHAR2(50) NOT NULL,
+    writer_name VARCHAR2(50) NOT NULL,
+    title VARCHAR2(255) NOT NULL,
+    regdate DATE NOT NULL,
+    moddate DATE NOT NULL,
+    read_cnd NUMBER,
+    PRIMARY KEY (article_no)
+);
 
 
 
