45d97edffed2fea0e83823450069bb145a9aaf18
sebaek <sebaek@gmail.com>
2020-12-10 17:06:16 +0900

    create table

--- WebContent/WEB-INF/sql/myboard.sql
+++ WebContent/WEB-INF/sql/myboard.sql
@@ -6,3 +6,24 @@ CREATE TABLE member (
     regdate DATE NOT NULL
 );
 SELECT * FROM member;
+
+CREATE TABLE article (
+    article_no NUMBER GENERATED AS IDENTITY,
+    writer_id VARCHAR2(50) NOT NULL,
+    writer_name VARCHAR2(50) NOT NULL,
+    title VARCHAR2(255) NOT NULL,
+    regdate DATE NOT NULL,
+    moddate DATE NOT NULL,
+    read_cnd NUMBER,
+    PRIMARY KEY (article_no)
+);
+
+CREATE TABLE article_content (
+    article_no NUMBER PRIMARY KEY,
+    content VARCHAR2(4000)
+);
+
+
+
+
+
